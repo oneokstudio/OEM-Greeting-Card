ad909e1505fe0146f39fbc5fbb2b9f813933f8ee
wangrun <yeesunday@gmail.com>
2015-09-19 16:28:04 +0800

    x

--- js/index.js
+++ js/index.js
@@ -1,35 +1,11 @@
 $(function () {
   var windowH = $(window).height();
   $('.wrapper').height(windowH);
+  $('.text').css({top: (windowH - $('.text').height()) / 2});
 
-  $('.sidebar').octoberLeaves({
-    leafStyles: 1,
-    numberOfLeaves: 2,
-    cycleSpeed: 40
-  })
-  $('.sidebar').octoberLeaves('start');
-
-  var animate = $('.cloud');
-  function loopbackground() {
-    animate.css('background-position', '0px 0px');
-    $({position_x: 0, position_y: 0}).animate({position_x: -5000, position_y: 0}, {
-      duration: 600000,
-      easing: 'linear',
-      step: function() {
-        animate.css('background-position', this.position_x+'px '+this.position_y+'px');
-      },
-      complete: function() {
-        loopbackground();
-      }
-    });
-  }
-  loopbackground();
-
-  //$('#lake-img').lake({
-  //  'speed': 1,
-  //  'scale': 0.5,
-  //  'waves': 10
-  //});
+  $('.poem').click(function () {
+    startCarousel();
+  });
   $('#menu1').click(function () {
     $('.text').html('能效+ 工业智造的4大价值体现');
   });
@@ -39,4 +15,67 @@ $(function () {
   $('#menu3').click(function () {
     $('.text').html('智能工厂/设备');
   });
+
+  animatePoem();
+  startRipples();
+  startCloud();
+  startLeaves();
+
+
+  function startRipples () {
+    $('.lake').ripples({
+      resolution: 100,
+      dropRadius: 20,
+      perturbance: 0.04,
+      interactive: true
+    });
+
+    setInterval(function() {
+      var $el = $('.lake');
+      var x = Math.random() * $el.outerWidth();
+      var y = Math.random() * $el.outerHeight();
+      var dropRadius = 20;
+      var strength = 0.04 + Math.random() * 0.04;
+
+      $el.ripples('drop', x, y, dropRadius, strength);
+    }, 1500);
+  }
+  function startLeaves () {
+    $('.sidebar').octoberLeaves({
+      leafStyles: 1,
+      numberOfLeaves: 2,
+      cycleSpeed: 40
+    })
+    $('.sidebar').octoberLeaves('start');
+  }
+  function startCloud () {
+    var animate = $('.cloud');
+    function loopbackground() {
+      animate.css('background-position', '0px 0px');
+      $({position_x: 0, position_y: 0}).animate({position_x: -5000, position_y: 0}, {
+        duration: 600000,
+        easing: 'linear',
+        step: function() {
+          animate.css('background-position', this.position_x+'px '+this.position_y+'px');
+        },
+        complete: function() {
+          loopbackground();
+        }
+      });
+    }
+    loopbackground();
+  }
+  function animatePoem() {
+    $('.poem').children().each(function (index) {
+      var $this = $(this);
+      setTimeout(function () {
+        //$this.animate({
+        //  marginLeft: '0',
+        //  opacity: 1
+        //}, 1500);
+        $this.fadeIn('slow');
+      }, 1500*index);
+      console.log(index);
+    })
+  }
 })
--- js/photos.js
+++ js/photos.js
@@ -1,27 +1,29 @@
-//$(function () {
+function startCarousel () {
   var owl = $("#carouselContainer");
   var currentIndex = 3;
   var currentSrc = '';
   var $imgDetail = $('#imgDetail');
   var $currentImgDom = $imgDetail.find('.current-img');
 
+  $('.poem').hide();
+  owl.show();
   owl.owlCarousel({
     loop: true,
     autoplay: true,
     items: 1,
-    lazyLoad:true,
+    lazyLoad: true,
     autoplayTimeout: 3000
   });
   owl.on('drag.owl.carousel', function (e) {
     owl.trigger('stop.owl.autoplay')
   });
-  owl.on('changed.owl.carousel',function(property){
+  owl.on('changed.owl.carousel', function (property) {
     currentIndex = property.item.index;
     currentSrc = $(property.target).find(".owl-item").eq(currentIndex).find("img").attr('src');
     showImgDetail();
   });
 
-  $('.owl-item').on('click', function(event){
+  $('.owl-item').on('click', function (event) {
     owl.trigger('pause.owl.autoplay');
     showImgDetail();
     $imgDetail.show();
@@ -40,11 +42,10 @@
     console.log('img dom');
   });
 
-  function showImgDetail () {
+  function showImgDetail() {
     $currentImgDom.html('<img src="' + currentSrc + '" alt="">');
     //setTimeout(function () {
     //  $currentImgDom.css({top: ($imgDetail.height() - $currentImgDom.find('img').height()) / 2});
     //}, 100);
   }
-
-//})
+}
