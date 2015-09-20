6cd8e4b6cca34e378b4821ee6d91a3cc9bb73455
sysumeepo <sysumeepo@gmail.com>
2015-09-20 23:07:20 +0800

    change sql

--- backend/oem.sql
+++ backend/oem.sql
@@ -1,18 +1,78 @@
-CREATE TABLE IF NOT EXISTS `feedbakcs` (
-  `id` INT(11) NOT NULL AUTO_INCREMENT,
-  `opt1` VARCHAR(10) NOT NULL,
-  `opt2` VARCHAR(10) NOT NULL,
-  `advice` TEXT,
-  `ctime` INT(11),
-  PRIMARY KEY(`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+-- phpMyAdmin SQL Dump
+-- version 4.4.13.1
+-- http://www.phpmyadmin.net
+--
+-- Host: 127.0.0.1
+-- Generation Time: 2015-09-20 15:06:53
+-- 服务器版本： 5.6.26
+-- PHP Version: 5.5.24
 
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `oem`
+--
+
+-- --------------------------------------------------------
+
+--
+-- 表的结构 `cards`
+--
 
 CREATE TABLE IF NOT EXISTS `cards` (
-  `id` INT(11) NOT NULL,
-  `sender` VARCHAR(32) NOT NULL,
-  `receiver` VARCHAR(32) NOT NULL,
-  `words` TEXT NOT NULL,
-  `ctime` INT(11),
-  PRIMARY KEY (`id`)
+  `id` int(11) NOT NULL,
+  `sender` varchar(32) NOT NULL,
+  `receiver` varchar(32) NOT NULL,
+  `words` text NOT NULL,
+  `ctime` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- 表的结构 `feedbacks`
+--
+
+CREATE TABLE IF NOT EXISTS `feedbacks` (
+  `id` int(11) NOT NULL,
+  `opt1` varchar(10) NOT NULL,
+  `opt2` varchar(10) NOT NULL,
+  `advice` text NOT NULL,
+  `ctime` int(11) NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `cards`
+--
+ALTER TABLE `cards`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `feedbacks`
+--
+ALTER TABLE `feedbacks`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `feedbacks`
+--
+ALTER TABLE `feedbacks`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
